1efa00c5dfbc88e7c17d8d0afcd0f501e29bdbb7
Jodeci <joedpacibe@gmail.com>
2025-12-15 20:00:12 +0800

    added wayfinder imports

--- vite.config.build.ts
+++ vite.config.build.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
+import {wayfinder} from "@laravel/vite-plugin-wayfinder";
 
 export default defineConfig({
     plugins: [
