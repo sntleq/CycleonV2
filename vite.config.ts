632c402f6abe7a8e4fad02b2d212b05f600f8f51
Jodeci <joedpacibe@gmail.com>
2025-12-15 18:37:35 +0800

    added @ alias

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,14 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
+import * as path from "node:path";
 
 export default defineConfig({
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'resources/js'),
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.tsx'],
